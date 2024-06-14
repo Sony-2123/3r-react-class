f109a54b1666c6e65b70855f06338fa352415fea
Sony-2123 <sonyreddy7406@gmail.com>
2024-06-14 18:19:49 +0530

    june 14

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-//import Images from "./components/image.js";
+import Images from "./components/image.js";
 import Listcomponent from "./components/list.js";
-// import TableComponent from "./components/table.js";
-// import FormsComponent from "./components/forms.js";
+ import TableComponent from "./components/table.js";
+ import FormsComponent from "./components/forms.js";
 import ButtonComponent from "./components/button.js";
-import Heading1,{Heading2,Heading3,Heading4,Heading5} from "./components/heading.js";
-
-function Hello(){
-  const isSubscribed=false
-  return(
-    <div>
-      <Heading1/>
-      <Heading2/>
-      <Heading3/>
-      <Heading4/>
-      <Heading5/>
-    </div>
-  )
-}
-export default Hello
+// import Heading1,{Heading2,Heading3,Heading4,Heading5} from "./components/heading.js";
+// function Hello(){
+//   const isSubscribed=false
+//   return(
+//     <div>
+//       <Heading1/>
+//       <Heading2/>
+//       <Heading3/>
+//       <Heading4/>
+//       <Heading5/>
+//     </div>
+//   )
+// }
+// export default Hello
 
 // function Hello(){
+//   return
 //    const employee={
 //     name:"sony",
 //    secondName:"ram"
 //   }
 //   const{name,secondName}=employee
-// return(
-
-
+// }
+//  export default Hello
+// function Hello(){
+//   return(
 //   <div className="nemm">
 //     <h1>Helloworld {name}</h1>
 //     <h1>Helloworld {secondName}</h1>
@@ -36,10 +37,10 @@ export default Hello
 // )
  
 // }
-//export default Hello
+// export default Hello
 // ------------------------------------------------------
 // function Hello() {
-//   const isSubscribed=false
+//   const isSubscribed=true
 //   if (isSubscribed) {
 //     return <h1>Thanks for Subscribing</h1>
     
@@ -53,14 +54,14 @@ export default Hello
 //-------------------------------------------------------
 //COMPONENT return
 // function Hello() {
-//   const isSubscribed=false
-  // if (isSubscribed) {
-  //   return <Listcomponent/>
+//   const isSubscribed=true
+  //if (isSubscribed) {
+//     return <Listcomponent/>
     
-  // }else{
-  //   return<h1>please Subscribe</h1>
-  // }
-//(or)
+//   }else{
+//     return<h1>please Subscribe</h1>
+//   }}
+// //(or)
 //   return(
 //     <div> 
 //       {
